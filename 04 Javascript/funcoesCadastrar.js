0df2da997e02d8a6e980a45319d108b046d56a32
Felipe-Dias-Mendes <48762331+Felipe-Dias-Mendes@users.noreply.github.com>
2019-05-12 18:25:53 -0300

    Erro

--- 04 Javascript/funcoesCadastrar.js
+++ 04 Javascript/funcoesCadastrar.js
@@ -10,9 +10,8 @@ function validarText(campo, alerta, label){
 		campo.classList.add("is-invalid");
 
 		campo.focus();
-		campo.value = "";
 		return false;
-
+	}
 	//Tudo corrento
 
 	document.getElementById(alerta).style.display="none";
@@ -25,11 +24,41 @@ function validarText(campo, alerta, label){
 	return true;
 }
 
+function validarNumero(campo, alerta, label){
+	var n = parseFloat(campo.value);
+
+	if(campo.value.length == 0 || isNaN(n)){
+		//Erro
+		document.getElementById(alerta).style.display = "block";
+		document.getElementById(label).classList.add("text-danger");
+
+		campo.classList.add("is-invalid");
+
+		campo.focus();
+		return false;
+	}
+	//Tudo corrento
+
+	document.getElementById(alerta).style.display="none";
+	campo.classList.remove("is-invalid");
+	campo.classList.add("is-valid");
+
+	document.getElementById(label).classList.remove("text-danger");
+	document.getElementById(label).classList.add("text-success");
+	return true;
+
+}
+
 function validar(){
 	var nome = document.dados.nome;
 	var descricao = document.dados.descricao;
 
-	validarText(nome, "alertaNome", "labelNome");
-	validarText(descricao, "alertaDescricao", "labelDescricao");
+	if(validarText(nome, "alertaNome", "labelNome") && 
+		validarText(descricao, "alertaDescricao", "labelDescricao") &&
+		validarText(codigo, "alertaCodigo", "labelCodigo")/*Continua...*/ ){
+
+	}
+	
+	
 
 }
