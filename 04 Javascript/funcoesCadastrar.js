2f168546aa82822d683eca862b49b27da907e583
Felipe-Dias-Mendes <48762331+Felipe-Dias-Mendes@users.noreply.github.com>
2019-05-14 16:31:09 -0300

    Continua Js...

--- 04 Javascript/funcoesCadastrar.js
+++ 04 Javascript/funcoesCadastrar.js
@@ -49,13 +49,49 @@ function validarNumero(campo, alerta, label){
 
 }
 
+function validarSelect(campo, alerta, label){
+	var n = parseFloat(campo.value);
+
+	if(campo.value == 0){
+		//Erro
+		document.getElementById(alerta).style.display = "block";
+		document.getElementById(label).classList.add("text-danger");
+
+		campo.classList.add("is-invalid");
+
+		campo.focus();
+		return false;
+	}
+	//Tudo corrento
+
+	document.getElementById(alerta).style.display="none";
+	campo.classList.remove("is-invalid");
+	campo.classList.add("is-valid");
+
+	document.getElementById(label).classList.remove("text-danger");
+	document.getElementById(label).classList.add("text-success");
+	return true;
+
+}
+
 function validar(){
 	var nome = document.dados.nome;
 	var descricao = document.dados.descricao;
+	var codigo = document.dados.codigo;
+	var categoria = document.dados.categoria;
+	var quant = document.dados.quantidade;
+	var medida = document.dados.medida;
+	var custo = document.dados.precocusto;
+
 
 	if(validarText(nome, "alertaNome", "labelNome") && 
-		validarText(descricao, "alertaDescricao", "labelDescricao") &&
-		validarText(codigo, "alertaCodigo", "labelCodigo")/*Continua...*/ ){
+		validarText(descricao, "alertaDescricao", "labelDescricao") && 
+		validarNumero(codigo, "alertaCodigo", "labelCodigo") &&
+		validarSelect(categoria, "alertaCategoria", "labelCategoria") &&
+		validarNumero(quant, "alertaQuant", "labelQuant") &&
+		validarText(medida, "alertaUni", "labelUni") &&
+		validarText(custo, "alertaCusto", "labelCusto")
+		/*Continua...*/ ){
 
 	}
 	
